782f3b1e506deff3e067d0a269d33933074bf95e
Abhinay07 <abhinaydoulagar07@gmail.com>
2022-10-02 13:17:29 +0530

    For loops

---
+++ src/test/ForLoopExamples.java
@@ -0,0 +1,14 @@
+package test;
+
+public class ForLoopExamples {
+
+    public static void main(String[] args) {
+
+        for (int i= 0;i<=10;i++){
+            System.out.println(i);
+        }
+        // Define and initilize a variable with some value
+        // Condition check
+        // Iteration i++ or i--
+    }
+}
